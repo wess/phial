38aa28e6a8ad3e3121a7fc9be27495fe5f915d22
Wess Cope <wess@wess.local>
2024-07-23 12:43:41 -0400

    Version bump

--- mix.exs
+++ mix.exs
@@ -3,20 +3,22 @@ defmodule Phial.MixProject do
 
   @source_url "https://github.com/wess/phial"
   @description "Handful of helpers for plug applications. No frameworks."
-  @version File.read!("VERSION") |> String.trim()
+  @version "0.0.2"
 
   def project do
     [
       app: :phial,
+      description: @description,
       version: @version,
+      package: package(),
       elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       docs: [
-        main: "Plug.Cowboy",
+        main: "Phial",
         source_ref: "v#{@version}",
         source_url: @source_url,
-        extras: []
+        extras: ["README.md"]
       ],
     ]
   end
@@ -43,7 +45,8 @@ defmodule Phial.MixProject do
       {:jason, "~> 1.4.3"},
       {:plug_cowboy, "~> 2.7.1"},
       {:ecto_sql, "~> 3.11.3"},
-      {:httpoison, "~> 2.2.1"}
+      {:httpoison, "~> 2.2.1"},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
     ]
   end
 end
